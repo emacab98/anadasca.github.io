6c7364244afc1648a6a6dbea4702858c4243e96a
anadasca <anamdasca98@gmail.com>
2020-08-02 21:21:13 +0200

    Home, Sign_up, Sign_in

---
+++ Sign_in/sign_in.js
@@ -0,0 +1,43 @@
+function checkForm(){     
+
+    var request = new XMLHttpRequest();
+   
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username==""|| password==""){
+      alert("Please fill al the information!");
+      return;
+    }
+   
+    /*request.open('GET', 'https://pacific-stream-14038.herokuapp.com/user/'+ username+"/"+password , true)
+    request.onload = function() {
+    
+    if (request.status >= 200 && request.status < 400) {
+        var risposta = JSON.parse(this.response);
+          
+          //memorizzazione
+          localStorage.setItem("username", risposta.nickname);
+         //Reindirizzamento
+          window.location.href = "Feed.html";
+  
+         } 
+    else {
+        
+        alert("Wrong combination of username and password!");
+        } 
+  }
+  
+  request.send(); */
+   }
+  
+   function googleSignUp(){
+    //window.location.href='https://pacific-stream-14038.herokuapp.com/auth/begin';
+    alert("Questa funzione fa il login con google")
+  
+  } 
+  
+  function githubSignUp(){
+    //window.location.href='https://pacific-stream-14038.herokuapp.com/auth/begin';
+    alert("Questa funzione fa il login con github")
+  
+  }
